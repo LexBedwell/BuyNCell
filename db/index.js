8108d4fc398b31db1b9326f1e8e335f978fad7da
LexBedwell <LexBedwell@gmail.com>
2018-12-10 12:41:41 -0500

    updated seed data

--- db/index.js
+++ db/index.js
@@ -1,5 +1,8 @@
 const conn = require('./connection')
 
+const categories = require('./seed/categories')
+const products = require('./seed/products')
+
 const Categories = require('./models/Categories')
 const Images = require('./models/Images')
 const LineItems = require('./models/LineItems')
@@ -9,22 +12,25 @@ const Reviews = require('./models/Reviews')
 const Users = require('./models/Users')
 
 LineItems.belongsTo(Products)
-
 Orders.hasMany(LineItems)
-
 Products.hasMany(Reviews)
-
 Users.hasMany(Reviews)
 Users.hasMany(Orders)
-
 Products.belongsToMany(Categories, { through: 'ProductsCategories' })
 Categories.belongsToMany(Products, { through: 'ProductsCategories' })
 
-const syncAndSeed = () => {
-  conn.sync({force: true})
-    .then( () => Products.create({title: 'Regular Celery', description: 'Pretty regular stuff', price: 1.00, quantity: 99}))
-    .then( () => Categories.create({name: 'Regular'}))
-    .then( () => console.log('sync and seed complete'))
+const syncAndSeed = async () => {
+  try {
+    await conn.sync({force: true})
+    const [cel, chopCel, mincCel, preCel, preChopCel, preMincCel] = await Promise.all(products.map( product => Products.create(product)))
+    const [reg, pre] = await Promise.all(categories.map( category => Categories.create(category)))
+    const regProducts = [cel, chopCel, mincCel]
+    const preProducts = [preCel, preChopCel, preMincCel]
+    await regProducts.forEach( product => product.addCategories(reg))
+    await preProducts.forEach( product => product.addCategories(pre))
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 module.exports = {
---
+++ db/seed/categories.js
@@ -0,0 +1,8 @@
+module.exports = [
+  {
+    name: 'Regular'
+  },
+  {
+    name: 'Premium'
+  }
+]
