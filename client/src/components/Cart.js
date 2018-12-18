2ae16fd1c51dde9d063dd41887ccdcab2faf5055
Lex Bedwell <LexBedwell@gmail.com>
2018-12-17 22:34:22 -0500

    added form to Cart component

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -3,28 +3,52 @@ import {connect} from 'react-redux'
 
 class Cart extends React.Component {
   render(){
-    if (Object.keys(this.props.cart).length === 0){
+    if (!this.props || this.props.lineItems === undefined){
       return null
     }
-    if (this.props.cart.lineItems.length === 0){
+    if (this.props.lineItems.length === 0 || this.state.lineItems.length === 0){
       return <h6>Cart Empty!</h6>
     }
     return (
-      <ul>
-        {this.props.cart.lineItems.map( lineItem => <li key={lineItem.id}>{lineItem.product.name} Quantity: {lineItem.quantity}</li>)}
-      </ul>
+      <form id="addToCart" onSubmit={this.handleSubmit}>
+        {this.state.lineItems.map( lineItem => {
+          let index = this.props.lineItems.indexOf(lineItem)
+          return (
+            <p key={lineItem.id}><strong>Name: </strong>{lineItem.product.name} // <strong>Quantity: </strong><input value={this.state.lineItems[index].quantity} onChange={this.handleChange} name={index.toString()} /></p>
+          )
+        })}
+      <p><button type="submit" className="btn btn-primary">Update Cart!</button></p>
+      </form>
     )
   }
   constructor(){
     super()
     this.state = {
-      cart: this.props ? this.props.cart : {}
+      lineItems: []
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(ev){
+    let newLineItems = this.state.lineItems
+    let parsed = parseInt(ev.target.name, 10)
+    newLineItems[parsed].quantity = ev.target.value
+    this.setState({
+      lineItems: newLineItems
+    })
+  }
+  handleSubmit(ev){
+    ev.preventDefault()
+  }
+  componentDidMount(){
+    if (this.props){ 
+      this.setState({lineItems: this.props.lineItems})
     }
   }
 }
 
-const mapStateToProps = (state) => {
-  return state
+const mapStateToProps = ({cart}) => {
+  return cart
 }
 
 module.exports = connect(mapStateToProps)(Cart)
