1a802479ff299207cd100adc30cf54afa9dc59d7
Lex Bedwell <LexBedwell@gmail.com>
2018-12-25 14:38:35 -0500

    removed comments

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -65,37 +65,3 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header)
-
-/*
-        <div className="col-2">
-            <Link className ="btn btn-outline-success btn-sm" to='/'>Home</Link>
-          </div>
-          <div className="col-2">
-            <Link className ="btn btn-outline-success btn-sm" to='/products'>Products</Link>
-          </div>
-          <div className="col-2">
-            <Link className="btn btn-outline-success btn-sm" to='/cart'>Cart</Link>
-          </div>
-          <div className="col-2">
-            {
-            localStorage.getItem('token') ? (
-              <div>
-                <Link className="btn btn-outline-success btn-sm" to='/orderhistory'>Orders</Link>
-              </div>
-            ) : (
-              ''
-            )
-          }
-          </div>
-          <div className="col-4 d-flex flex-row-reverse">
-          {
-            localStorage.getItem('token') ? (
-              <div>
-                <h6 className="text-dark">Hello <strong>{this.props.auth.githubUserId}</strong>! <button className="btn btn-outline-danger btn-sm" onClick={() => {this.props.logout(); this.props.history.push('/')}}>Logout</button></h6>
-              </div>
-            ) : (
-              <a href='/api/auth/github' className="btn btn-outline-success btn-sm">Login with Github</a>
-            )
-          }
-          </div>
-*/
