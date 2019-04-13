2e3cb5d9345f1c7348bf68d73dbca2a12568f069
Lex Bedwell <LexBedwell@gmail.com>
2019-04-12 23:37:56 -0400

    adjust splash formatting

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -3,14 +3,14 @@ import React from 'react'
 class Main extends React.Component{
   render(){
     return (
-      <table className="container w-75" style={{height: "100%"}}>
+      <table className="container w-75" style={{height: "80%"}}>
         <tbody>
           <tr>
             <td className="align-middle">
               <table>
                 <tbody>
                   <tr>
-                    <td className="title"><h2>Celery Store</h2></td>
+                    <td className="title"><h1>Celery Store</h1></td>
                   </tr>
                   <tr>
                     <td className="title text-right"><h6><em>We fresh</em></h6></td>
