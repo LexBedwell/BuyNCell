6a89046a287d46a6c5c1e41007d667589b3e4803
Lex Bedwell <LexBedwell@gmail.com>
2019-07-02 00:26:57 -0400

    fix typo

--- db/index.js
+++ db/index.js
@@ -32,7 +32,7 @@ const syncAndSeed = async () => {
     const sampleUsers = await Promise.all(users.map( user => Users.create(user)))
     const sampleOrders = await Promise.all(sampleUsers.map( user => Orders.create({userId: user.id, addressName: user.addressName, addressLine: user.addressLine, addressCity: user.addressCity, addressState: user.addressState, addressZip: user.addressZip})))
     await Promise.all(regProducts.concat(preProducts).map( product => LineItems.create({productId: product.id, orderId: sampleOrders[0].id})))
-    console.info('Databse syncAndSeed completed.')
+    console.info('Databse syncAndSeed completed')
   } catch (err) {
     console.error(err.message)
   }
