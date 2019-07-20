03e75c52a9497973c7f1f5986d72c9262cb1c869
Lex Bedwell <LexBedwell@gmail.com>
2019-07-20 00:08:04 -0400

    add pdp inventory check

--- api/routes/inventory.js
+++ api/routes/inventory.js
@@ -6,7 +6,7 @@ router.get('/:id', (req, res, next) => {
   axios.get( (process.env.INVENTORY_SERVICE_URL || 'https://celery-store-inventory-service.herokuapp.com') + '/inventory/' + req.params.id )
     .then( response => {
       if (response.data.error) { 
-        res.send({isInStock: response.data.error})
+        res.send({error: response.data.error})
       } else {
         res.send({isInStock: response.data[req.params.id] || false})
       }
