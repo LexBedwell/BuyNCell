5ffd200bdf00e881c38bea713fc0d94689135b55
Lex Bedwell <LexBedwell@gmail.com>
2019-08-03 20:50:07 -0400

    BE done

--- api/routes/inventory.js
+++ api/routes/inventory.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const axios = require('axios')
 
 router.get('/:id', (req, res, next) => {
-  axios.get( (process.env.INVENTORY_SERVICE_URL || 'https://celery-store-inventory-service.herokuapp.com') + '/inventory/' + req.params.id )
+  axios.get( (process.env.INVENTORY_SERVICE_URL || 'https://celery-store-inventory-service.herokuapp.com') + '/inventory' + req.params.id )
     .then( response => {
       if (response.data.error) { 
         res.send({error: response.data.error})
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const axios = require('axios')
 
 const {Op} = require('sequelize')
 
@@ -96,29 +97,37 @@ router.put('/submit', async (req, res, next) => {
       let searchUser = await models.Users.findByPk(req.body.userId)
       orderEmail = searchUser.email
     }
-    req.body.lineItems.forEach(async lineItem => {
-      await models.LineItems.update({
-        quantity: lineItem.quantity
-      }, {
+    let inventoryServiceOrder = {}
+    req.body.lineItems.forEach( elem => inventoryServiceOrder[elem.id] = elem.quantity)
+    let inventoryServiceResponse = await axios.put(process.env.INVENTORY_SERVICE_URL + '/inventory' || 'https://celery-store-inventory-service.herokuapp.com/inventory', inventoryServiceOrder)
+    if (inventoryServiceResponse.data.processTransaction === true) {
+      console.info('transaction processed')
+      req.body.lineItems.forEach(async lineItem => {
+        await models.LineItems.update({
+          quantity: lineItem.quantity
+        }, {
+          where: {
+              id: lineItem.id
+          }
+        })
+      })
+      await models.Orders.update({
+          status: 'processing',
+          isPaid: true,
+          addressName: req.body.addressName,
+          addressLine: req.body.addressLine,
+          addressCity: req.body.addressCity,
+          addressState: req.body.addressState,
+          addressZip: req.body.addressZip
+        }, {
         where: {
-            id: lineItem.id
+          id: req.body.id
         }
       })
-    })
-    await models.Orders.update({
-        status: 'processing',
-        isPaid: true,
-        addressName: req.body.addressName,
-        addressLine: req.body.addressLine,
-        addressCity: req.body.addressCity,
-        addressState: req.body.addressState,
-        addressZip: req.body.addressZip
-      }, {
-      where: {
-        id: req.body.id
-      }
-    })
-    sendConfirmationEmail(orderId, orderEmail)
+      sendConfirmationEmail(orderId, orderEmail)
+    } else {
+      console.info('transaction denied - no inventory')
+    }
     res.sendStatus(200)
   } catch (err) {
     next(err)
